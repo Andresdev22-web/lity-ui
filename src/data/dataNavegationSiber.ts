6fc1af5c271128030be8b21c86239446daf5f6cc
Tu AndresNana21 <andresgbarreracortes@gmail.com>
2026-08-22 23:41:17 -0500

    una vance :b

---
+++ src/data/dataNavegationSiber.ts
@@ -0,0 +1,16 @@
+export const docsNavSections = [
+  {
+    title: 'Primeros Pasos',
+    items: [
+      { label: 'Introducción', href: '/documentation', icon: 'lucide:sparkles' },
+      { label: 'Instalación', href: '/documentation/instalacion', icon: 'lucide:terminal' },
+    ],
+  },
+  {
+    title: 'Layouts',
+    items: [
+      { label: 'Documentacion', href: '/documentation/layout/documentacion', icon: 'lucide:layout-template' },
+       { label: 'landing Page', href: '/documentation/layout/landingPage', icon: 'lucide:layout-template' },
+    ],
+  },
+];
